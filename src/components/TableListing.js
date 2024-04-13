aaff73550ce750e7d249cafc3771b242643e21a8
jendoki <lundoki@gmail.com>
2024-04-13 15:18:19 +0200

    Remove sorting on table

--- src/components/TableListing.js
+++ src/components/TableListing.js
@@ -1,55 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import ListElement from "./ListElement";
 
 function TableListing(props) {
-    const [sortColumn, setSortColumn] = useState(null);
-    const [sortDirection, setSortDirection] = useState('asc');
-
-    const handleSort = (column) => {
-        if (sortColumn === column) {
-            setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
-        } else {
-            setSortColumn(column);
-            setSortDirection('asc');
-        }
-    };
-
-    const sortedListElements = [...props.listElements].sort((a, b) => {
-        if (!sortColumn) return 0;
-
-        const aValue = a[sortColumn];
-        const bValue = b[sortColumn];
-
-        if (aValue === undefined || bValue === undefined) return 0;
-
-        if (aValue < bValue) {
-            return sortDirection === 'asc' ? -1 : 1;
-        }
-        if (aValue > bValue) {
-            return sortDirection === 'asc' ? 1 : -1;
-        }
-        return 0;
-    });
-
     return (
         <div className="list-container">
             {props.listTitle && <h2>{props.listTitle}</h2>}
             <table className="list-table">
                 <thead>
                     <tr className="list-table-titles">
-                        {props.listTitles && props.listTitles.length > 0 ? props.listTitles.map(title =>
-                            <th key={title} onClick={() => handleSort(title)}>
-                                {title} {sortColumn === title && (
-                                    sortDirection === 'asc' ? <p>&uarr;</p> : <p>&darr;</p>
-                                )}
-                            </th>
-                        ) : console.warn("Please provide a list of titles to TableListing with the listTitles props.")}
+                        {props.listTitles && props.listTitles.length > 0 ? props.listTitles.map(title => <th key={title}>{title}</th>) : console.warn("Please provide a list of titles to TableListing with the listTitles props.")}
                     </tr>
                 </thead>
                 <tbody>
-                    {sortedListElements && sortedListElements.length > 0 ? sortedListElements.map((element, index) =>
-                        <ListElement key={index} element={element} />
-                    ) : console.warn("Please provide a list of elements to TableListing with the listElements props.")}
+                    {props.listElements && props.listElements.length > 0 ? props.listElements.map(element => <ListElement key={element.id} element={element} />) : console.warn("Please provide a list of elements to TableListing with the listElements props.")}
                 </tbody>
             </table>
         </div>
