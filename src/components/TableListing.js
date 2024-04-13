9e0b180ec2e24ac220147d14415412593b7c6b58
jendoki <lundoki@gmail.com>
2024-04-13 14:10:51 +0200

    Update TableListing with warning cases

--- src/components/TableListing.js
+++ src/components/TableListing.js
@@ -3,17 +3,18 @@ import ListElement from "./ListElement";
 
 function TableListing(props) {
     console.log("props in TableListing", props)
+
     return (
         <div className="list-container">
-            <h2>{props.listTitle}</h2>
+            {props.listTitle && <h2>{props.listTitle}</h2>}
             <table className="list-table">
                 <thead>
                     <tr className="list-table-titles">
-                        {props.listTitles.map(title => <th>{title}</th>)}
+                        {props.listTitles > 0 ? props.listTitles.map(title => <th>{title}</th>) : console.warn("Please provide a list of titles to TableListing with the listTitles props.")}
                     </tr>
                 </thead>
                 <tbody>
-                    {props.listElements.map(element => <ListElement element={element} />)}
+                    {props.listElements > 0 ? props.listElements.map(element => <ListElement element={element} />) : console.warn("Please provide a list of elements to TableListing with the listElements props.")}
                 </tbody>
             </table>
         </div>
